dd827ea089f12a4da03b7630fc78c4a3333d840d
snehajoshi90 <snehalvaidya19@gmail.com>
2024-03-22 16:54:09 +0530

    photo list screen

---
+++ lib/screens/photos_list_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class PhotosListScreen extends StatelessWidget {
+  const PhotosListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title:const Text('Photos list'),),
+    );
+  }
+}

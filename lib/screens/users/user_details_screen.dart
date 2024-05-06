c757ecdb38b3eecebbc06dbbb28d834c91d08bc5
snehajoshi90 <snehalvaidya19@gmail.com>
2024-05-06 17:31:03 +0530

    removed row widget from user details screen

--- lib/screens/users/user_details_screen.dart
+++ lib/screens/users/user_details_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'model/users_response.dart';
@@ -14,164 +15,112 @@ class UserDetailsScreen extends StatelessWidget {
         title: Text('User Details'),
       ),
       body: SingleChildScrollView(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              SizedBox(
-                height: 20,
-              ),
-              Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Card(
-                  child: Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Text(
-                          'User ID: ${usersResponse.id}',
-                          style: Theme.of(context).textTheme.bodyMedium,
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        Row(
-                          children: [
-                            const Text('Name: '),
-                            Text(
-                              ' ${usersResponse.name}',
-                              style: Theme.of(context).textTheme.titleLarge,
-                            ),
-                          ],
-                        ),
-                        Row(
-                          children: [
-                            Text('Use Name: ${usersResponse.username}'),
-                          ],
-                        ),
-                        const SizedBox(
-                          height: 20,
-                        ),
-                        Text('Address',
-                            style: Theme.of(context).textTheme.bodyMedium),
-                        const SizedBox(
-                          height: 20,
-                        ),
-                        Row(
-                          children: [
-                            Text('Suite: ${usersResponse.address!.suite}',maxLines: 3,),
-                            const SizedBox(
-                              width: 10,
-                            ),
-                            Text('Street: ${usersResponse.address!.street}'),
-                          ],
-                        ),
-                        Row(
-                          children: [
-                            Text('City: ${usersResponse.address!.city}'),
-                          ],
-                        ),
-                        Row(
-                          children: [
-                            Text(
-                                'Latitude: ${usersResponse.address!.geo!.lat}'),
-                            const SizedBox(
-                              width: 10,
-                            ),
-                          ],
-                        ),
-                        Row(
-                          children: [
-                            Text(
-                                'Longitude: ${usersResponse.address!.geo!.lng}'),
-                          ],
-                        ),
-                        Row(
-                          children: [
-                            Text('Zipcode: ${usersResponse.address!.zipcode}'),
-                          ],
-                        ),
-                        SizedBox(
-                          height: 20,
-                        ),
-                        Row(
-                          children: [
-                            Text('Email: ${usersResponse.email}'),
-                          ],
-                        ),
-                        Row(
-                          children: [
-                            const Text('Phone: '),
-                            Text(
-                              ' ${usersResponse.phone}',
-                              style: TextStyle(fontWeight: FontWeight.bold),
-                            ),
-                          ],
-                        ),
-                        Row(
-                          children: [
-                            Text('Website: ${usersResponse.website}'),
-                          ],
-                        ),
-                      ],
-                    ),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    // mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'User ID: ${usersResponse.id}',
+                        style: Theme.of(context).textTheme.bodyMedium,
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                        children: [
+                          const Text('Name: '),
+                          Text(
+                            ' ${usersResponse.name}',
+                            style: Theme.of(context).textTheme.titleLarge,
+                          ),
+                        ],
+                      ),
+                      Text('Use Name: ${usersResponse.username}'),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Text('Address',
+                          textAlign: TextAlign.center,
+                          style: Theme.of(context).textTheme.bodyMedium),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        'Suite: ${usersResponse.address!.suite}',
+                        //maxLines: 3,
+                      ),
+                      Text('Street: ${usersResponse.address!.street}'),
+                      Text('City: ${usersResponse.address!.city}'),
+                      Text('Latitude: ${usersResponse.address!.geo!.lat}'),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      Text('Longitude: ${usersResponse.address!.geo!.lng}'),
+                      Text('Zipcode: ${usersResponse.address!.zipcode}'),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Text('Email: ${usersResponse.email}'),
+                      Row(
+                        children: [
+                          const Text('Phone: '),
+                          Text(
+                            ' ${usersResponse.phone}',
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
+                      Text('Website: ${usersResponse.website}'),
+                    ],
                   ),
                 ),
               ),
-              Padding(
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Card(
+                  child: Padding(
                 padding: const EdgeInsets.all(20.0),
-                child: Card(
-                    child: Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Column(
-                    children: [
-                      Row(
-                        children: [
-                          const Text('Company Name: '),
-                          Text(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        const Text('Company Name: '),
+                        Expanded(
+                          child: Text(
                             ' ${usersResponse.company!.name}',
                             style: const TextStyle(fontWeight: FontWeight.bold),
-                            maxLines: 3,
+                            // maxLines: 3,
                           ),
-                        ],
-                      ),
-                      Row(
-                        children: [
-                          Text('BS: ${usersResponse.company!.bs}'),
-                        ],
-                      ),
-                      Row(
-                        children: [
-                          Expanded(
-                            child: Text(
-                              'Catch Phrase: ${usersResponse.company!.catchPhrase}',
-                              maxLines: 2,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                )),
-              ),
-            ],
-          ),
+                        ),
+                      ],
+                    ),
+                    Text(
+                      'BS: ${usersResponse.company!.bs}',
+                      maxLines: 3,
+                    ),
+                    Text(
+                      'Catch Phrase: ${usersResponse.company!.catchPhrase}',
+                      maxLines: 3,
+                    ),
+                  ],
+                ),
+              )),
+            ),
+          ],
         ),
       ),
     );
-    //Column(
-    //     mainAxisAlignment: MainAxisAlignment.center,
-    //    // crossAxisAlignment: CrossAxisAlignment.center,
-    //     children: [
-    //       Row(
-    //         mainAxisAlignment: MainAxisAlignment.center,
-    //         children: [
-    //           Text('${usersResponse.name}User name'),
-    //         ],
-    //       )
-    //     ],
-    //   ),
-    // );
   }
 }
